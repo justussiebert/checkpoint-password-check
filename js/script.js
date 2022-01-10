7e89311f00e82b2a997e965fa31264b3c5cff6c9
Justus Siebert <info@justus-siebert.de>
2022-01-10 11:40:07 +0100

    js: first check built (if passwords equal); built a state

--- js/script.js
+++ js/script.js
@@ -1,7 +1,31 @@
 //const myForm = document.querySelector(".formX");
 const inputPasswordField1 = document.querySelector("#input-password-1");
 const inputPasswordField2 = document.querySelector("#input-password-2");
+const inputPasswordFields = document.querySelector(".input-password");
 const buttonTogglePassword = document.querySelector("#button-toggle-password");
+
+const listItemPasswordsEqual = document.querySelector("#check-passwords-equal");
+const listItemPasswordsLowerCase = document.querySelector(
+  "#check-passwords-lower-case"
+);
+const listItemPasswordsUpperCase = document.querySelector(
+  "#check-passwords-upper-case"
+);
+const listItemPasswordsNumbers = document.querySelector(
+  "#check-passwords-numbers"
+);
+const listItemPasswordsMinLength = document.querySelector(
+  "#check-passwords-min-length"
+);
+
+const state = {
+  checkPasswordsEqual: false,
+  checkPasswordsLowerCase: false,
+  checkPasswordsUpperCase: false,
+  checkPasswordsNumbers: false,
+  checkPasswordsminLength: false,
+  PasswordsminLength: 10,
+};
 
 buttonTogglePassword.addEventListener("click", function (e) {
   e.preventDefault();
@@ -25,3 +49,26 @@ buttonTogglePassword.addEventListener("click", function (e) {
     buttonTogglePassword.value = "Passwort anzeigen";
   }
 });
+
+// if input on input-field passw1 or passw2
+[inputPasswordField1, inputPasswordField2].forEach(function (passwordField) {
+  passwordField.addEventListener("input", function (e) {
+    //alert(this.value);
+    checkPasswords();
+  });
+});
+
+function checkPasswords() {
+  //console.log(inputPasswordField1.value.length);
+  if (
+    inputPasswordField1.value === inputPasswordField2.value &&
+    inputPasswordField1.value.length > 0 &&
+    inputPasswordField2.value.length > 0
+  ) {
+    //console.log("beide gleich und min 1 zeichen");
+    state.checkPasswordsEqual = true;
+  } else {
+    state.checkPasswordsEqual = false;
+  }
+  console.log(state.checkPasswordsEqual);
+}
